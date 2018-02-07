19b04cf13cae86e1a22ab0ad83ec46fd5b3ce6fc
Tim Oliver <me@timoliver.com.au>
2018-02-07 17:05:58 +0800

    Fixed title label issues

--- TONavigationBar/TONavigationBar.m
+++ TONavigationBar/TONavigationBar.m
@@ -9,6 +9,8 @@
 #import "TONavigationBar.h"
 #import "UINavigationItem+TONavigationBar.h"
 
+#define UIKitLocalizedString(key) [[NSBundle bundleWithIdentifier:@"com.apple.UIKit"] localizedStringForKey:key value:@"" table:nil]
+
 @interface TONavigationBar () <UINavigationControllerDelegate>
 
 // The `UINavigationController` object that is governing this navigation bar
@@ -25,6 +27,9 @@
 
 // Fetch a reference to the title label so we can control it
 @property (nonatomic, readonly) UILabel *titleTextLabel;
+
+// An internal reference to the content view that holds all of visible subviews of the navigation bar
+@property (nonatomic, weak) UIView *contentView;
 
 @end
 
@@ -53,6 +58,9 @@
     [self setBackgroundImage:[UIImage new] forBarMetrics:UIBarMetricsDefault];
     [self setShadowImage:[UIImage new]];
     
+    // Now the views will have been laid down, capture the internal view that holds the labels
+    [self captureContentView];
+    
     // Update the views to match the current bar style
     [self updateContentViewsForBarStyle];
 }
@@ -78,6 +86,11 @@
     frame.origin.y = frame.size.height - _separatorHeight;
     frame.size.height = _separatorHeight;
     self.separatorView.frame = frame;
+    
+    //
+    if (self.backgroundHidden) {
+        self.titleTextLabel.hidden = YES;
+    }
 }
 
 - (void)updateContentViewsForBarStyle
@@ -97,12 +110,6 @@
     // Configure the separator color
     CGFloat greyColor = darkMode ? 0.8f : 0.4f;
     self.separatorView.backgroundColor = [UIColor colorWithWhite:greyColor alpha:1.0f];
-    
-    // If we've got a custom title view, configure the text color
-    if (self.topItem.titleView && [self.topItem.titleView isKindOfClass:[UILabel class]]) {
-        UILabel *titleLabel = (UILabel *)self.topItem.titleView;
-        titleLabel.textColor = (!darkMode ? [UIColor blackColor] : [UIColor whiteColor]);
-    }
 }
 
 #pragma mark - Transition Handling -
@@ -118,25 +125,34 @@
 
 - (void)setBackgroundHidden:(BOOL)hidden animated:(BOOL)animated forViewController:(UIViewController *)viewController
 {
+    // Regardless of the outcome, force a re-layout so we can ensure all of our controlled views are up-to-date
+    [self setNeedsLayout];
+    
     if (hidden == _backgroundHidden) {
         return;
     }
     
+    // An animation block that will handle transitioning all of the views during a 'non-hidden-to-hidden' animation
     void (^animationBlock)(BOOL) = ^(BOOL _hidden) {
         self.backgroundView.alpha = _hidden ? 0.0f : 1.0f;
         self.separatorView.alpha = _hidden ? 0.0f : 1.0f;
         self.tintColor = _hidden ? [UIColor whiteColor] : nil;
-        self.barStyle = hidden ? UIBarStyleBlack : self.preferredBarStyle;
-        
+
+        // iOS 11 is pretty broken. If this code isn't set, the title labels
+        // may sometimes fail to switch to the correct colour
         UIColor *textColor = (self.preferredBarStyle > UIBarStyleDefault) ? [UIColor whiteColor] : [UIColor blackColor];
-        self.titleTextLabel.textColor = textColor;
         self.titleTextAttributes = @{NSForegroundColorAttributeName : textColor};
         self.largeTitleTextAttributes = @{NSForegroundColorAttributeName : textColor};
     };
+
+    // A block for switching the bar style, separate from the animation block since there
+    // are times when it shouldn't be animated
+    void (^toggleBarStyleBlock)(void) = ^{
+        self.barStyle = hidden ? UIBarStyleBlack : self.preferredBarStyle;
+    };
     
+    // Set the new value
     _backgroundHidden = hidden;
-    
-    viewController.navigationItem.to_titleHidden = YES;
     
     // If no transition coordinator was supplied, defer back to a pre-canned animation.
     // For some annoying reason, the initial transition coordinator in iOS 11 fails to play the animation properly. (Possibly a UIKit bug)
@@ -148,19 +164,27 @@
         
         if (animated) {
             [UIView animateWithDuration:duration animations:^{
+                toggleBarStyleBlock();
                 animationBlock(hidden);
             }];
         }
         else {
+            toggleBarStyleBlock();
             animationBlock(hidden);
         }
         return;
     }
     
+    // Apparently parts of the status bar can fail to change color when captured in an interactive transition. So in thoses
+    // cases, simply flip the bar style outside of the block.
+    toggleBarStyleBlock();
+    
     // If we are in an interactive animation (eg, swipe-to-go-back in UINavigationController), attach the animations to the coordinator
     [transitionCoordinator animateAlongsideTransition:^(id<UIViewControllerTransitionCoordinatorContext> context) {
         animationBlock(hidden);
     } completion:^(id<UIViewControllerTransitionCoordinatorContext> context) {
+        [viewController setNeedsStatusBarAppearanceUpdate];
+        
         // If the transition cancelled (eg, the user swiped back, but let go too soon), restore to the previous state
         if (context.cancelled) {
             animationBlock(hidden);
@@ -169,32 +193,37 @@
 }
 
 #pragma mark - Internal View Traversal -
-- (UILabel *)titleTextLabel
+- (BOOL)captureContentView
 {
     for (UIView *subview in self.subviews) {
         if ([NSStringFromClass([subview class]) rangeOfString:@"Content"].location != NSNotFound) {
-            for (UIView *subSubview in subview.subviews) {
-                if ([subSubview isKindOfClass:[UILabel class]]) {
-                    return (UILabel *)subSubview;
-                }
-            }
+            self.contentView = subview;
+            return YES;
+        }
+    }
+    
+    return NO;
+}
+
+- (UILabel *)titleTextLabel
+{
+    // This is somewhat fragile as it relies on the internal ordering of the UINavigationBar subviews
+    // to catch the right one (unless Apple is performing manual layer ordering. In which case we're fine!)
+    // The title label is always the first `UILabel` in the `UINavigationBar` stack, once an animation has started.
+    for (UIView *subSubview in self.contentView.subviews) {
+        if ([subSubview isKindOfClass:[UILabel class]]) {
+            return (UILabel *)subSubview;
         }
     }
     
     return nil;
 }
 
-#pragma mark - Accessors -
-
-- (void)setBarStyle:(UIBarStyle)barStyle
+- (void)setPreferredBarStyle:(UIBarStyle)preferredBarStyle
 {
-    [super setBarStyle:barStyle];
+    if (_preferredBarStyle == preferredBarStyle) { return; }
+    _preferredBarStyle = preferredBarStyle;
     [self updateContentViewsForBarStyle];
-}
-
-- (UIBarStyle)barStyle
-{
-    return [super barStyle];
 }
 
 @end
